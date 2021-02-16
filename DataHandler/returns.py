a966774b1877e2d61ba143e56f1db5041a287a00
SarmadTanveer <59303670+SarmadTanveer@users.noreply.github.com>
2021-02-16 09:21:18 -0500

    Update returns.py

--- DataHandler/returns.py
+++ DataHandler/returns.py
@@ -5,11 +5,11 @@ import re
 def getPriceReturn(ticker,startingYear):
     endingYear = str(int(startingYear) + 1)
     HS_p = requests.get(
-        f"https://financialmodelingprep.com/api/v4/historical-price-full/{ticker}/{startingYear}-01-03?apikey=d684d9ac76fc9902f5608b588fff159b")
+        f"https://financialmodelingprep.com/api/v4/historical-price-full/{ticker}/{startingYear}-01-03?apikey=yourapirkey")
     HS_p = HS_p.json()
     print(json.dumps(HS_p))
     HS_1 = requests.get(
-        f"https://financialmodelingprep.com/api/v4/historical-price-full/{ticker}/{endingYear}-01-04?apikey=d684d9ac76fc9902f5608b588fff159b")
+        f"https://financialmodelingprep.com/api/v4/historical-price-full/{ticker}/{endingYear}-01-04?apikey=yourapikey")
     HS_1 = HS_1.json()
     print(json.dumps(HS_1))
     comissions = 20.00
